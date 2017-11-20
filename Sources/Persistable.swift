8e5bde7334e173cadf0c49ac2c000c575d521c1b
Maxim Volgin <maxim.volgin@me.com>
2017-11-20 18:29:23 +0100

    predicate as var

--- Sources/Persistable.swift
+++ Sources/Persistable.swift
@@ -11,27 +11,29 @@ import CoreData
 
 public protocol Persistable {
     associatedtype T: NSManagedObject
-    
+
     static var entityName: String { get }
-    
+
     /// The attribute name to be used to uniquely identify each instance.
     static var primaryAttributeName: String { get }
-    
+
     var identity: String { get }
 
     init(entity: T)
 
     func update(_ entity: T)
-    
+
     /* predicate to uniquely identify the record, such as: NSPredicate(format: "code == '\(code)'") */
-    func predicate() -> NSPredicate
-    
+    public var predicate: NSPredicate { get }
+
 }
 
 public extension Persistable {
-    
-    func predicate() -> NSPredicate {
-        return NSPredicate(format: "%K = %@", Self.primaryAttributeName, self.identity)
+
+    public var predicate: NSPredicate {
+        get {
+            return NSPredicate(format: "%K = %@", Self.primaryAttributeName, self.identity)
+        }
     }
 
 }
