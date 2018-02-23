e4d40802c3fa38d1523e67f476c8fbc657238d66
Simon Rice <im@simonrice.com>
2018-02-23 21:48:11 +0000

    Added mac 🖥, tv 📺 & watch ⌚️ support for Carthage

--- RxCoreData/RxCoreData.h
+++ RxCoreData/RxCoreData.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 RxSwiftCommunity. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for RxCoreData.
 FOUNDATION_EXPORT double RxCoreDataVersionNumber;
