df22c6d1ab2919b0b86d9adb6223d36e56aa4d99
Max Pfeiffer <max.pfeiffer@alp-phone.ch>
2018-05-11 11:35:56 +0200

    Remove identity property because it interferes with property from IdentifiableType protocol in RxDatasources. Objects which implement Peristable protocol need to implement IdentifiableType protocol as well. Otherwise cell updates won't work.  See: https://github.com/RxSwiftCommunity/RxDataSources/blob/master/Sources/Differentiator/IdentifiableType.swift

--- Sources/Persistable.swift
+++ Sources/Persistable.swift
@@ -17,8 +17,6 @@ public protocol Persistable {
     /// The attribute name to be used to uniquely identify each instance.
     static var primaryAttributeName: String { get }
     
-    var identity: String { get }
-
     init(entity: T)
 
     func update(_ entity: T)
@@ -26,12 +24,4 @@ public protocol Persistable {
     /* predicate to uniquely identify the record, such as: NSPredicate(format: "code == '\(code)'") */
     func predicate() -> NSPredicate
     
-}
-
-public extension Persistable {
-    
-    func predicate() -> NSPredicate {
-        return NSPredicate(format: "%K = %@", Self.primaryAttributeName, self.identity)
-    }
-
 }
