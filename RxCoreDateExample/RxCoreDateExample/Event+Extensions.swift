37236c3fa16c2b74a6d931e8bda2ff65f7c4188a
bobgodwinx <bobgodwinx@gmail.com>
2019-04-22 18:30:00 +0000

    Recreated the example file since the old would not build

---
+++ RxCoreDateExample/RxCoreDateExample/Event+Extensions.swift
@@ -0,0 +1,45 @@
+import Foundation
+import CoreData
+import RxDataSources
+import RxCoreData
+
+func == (lhs: Event, rhs: Event) -> Bool {
+    return lhs.id == rhs.id
+}
+
+extension Event : Equatable { }
+
+extension Event : IdentifiableType {
+    typealias Identity = String
+    
+    var identity: Identity { return id }
+}
+
+extension Event : Persistable {
+    typealias T = NSManagedObject
+    
+    static var entityName: String {
+        return "Event"
+    }
+    
+    static var primaryAttributeName: String {
+        return "id"
+    }
+    
+    init(entity: T) {
+        id = entity.value(forKey: "id") as! String
+        date = entity.value(forKey: "date") as! Date
+    }
+    
+    func update(_ entity: T) {
+        entity.setValue(id, forKey: "id")
+        entity.setValue(date, forKey: "date")
+        
+        do {
+            try entity.managedObjectContext?.save()
+        } catch let e {
+            print(e)
+        }
+    }
+    
+}
---
+++ RxCoreDateExample/RxCoreDateExample/ViewController.swift
@@ -0,0 +1,81 @@
+import UIKit
+import CoreData
+import RxSwift
+import RxCocoa
+import RxDataSources
+import RxCoreData
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addBarButtonItem: UIBarButtonItem!
+    
+    var managedObjectContext: NSManagedObjectContext!
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bindUI()
+        configureTableView()
+    }
+    
+    func bindUI() {
+        addBarButtonItem.rx.tap
+            .map { _ in
+                Event(id: UUID().uuidString, date: Date())
+            }.subscribe(onNext: { [weak self] (event) in
+                _ = try? self?.managedObjectContext.rx.update(event)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func configureTableView() {
+        tableView.isEditing = true
+        
+        /*
+         // Non-animated
+        
+         managedObjectContext.rx.entities(Event.self,
+                                          sortDescriptors: [NSSortDescriptor(key: "date", ascending: false)])
+            .bindTo(tableView.rx.items(cellIdentifier: "Cell")) { row, event, cell in
+                cell.textLabel?.text = "\(event.date)"
+            }
+            .addDisposableTo(disposeBag)
+        */
+        
+        // Animated
+
+        let animatedDataSource = RxTableViewSectionedAnimatedDataSource<AnimatableSectionModel<String, Event>>(configureCell: { dateSource, tableView, indexPath, event in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = "\(event.date)"
+            return cell
+        })
+        
+        managedObjectContext.rx.entities(Event.self, sortDescriptors: [NSSortDescriptor(key: "date", ascending: false)])
+            .map { events in
+                [AnimatableSectionModel(model: "Section 1", items: events)]
+            }
+            .bind(to: tableView.rx.items(dataSource: animatedDataSource))
+            .disposed(by: disposeBag)
+ 
+        self.tableView.rx.itemDeleted.map { [unowned self] ip -> Event in
+            return try self.tableView.rx.model(at: ip)
+            }
+            .subscribe(onNext: { [unowned self] (event) in
+                do {
+                    try self.managedObjectContext.rx.delete(event)
+                } catch {
+                    print(error)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        animatedDataSource.canEditRowAtIndexPath = { _,_  in
+            return true
+        }
+        animatedDataSource.canMoveRowAtIndexPath = { _,_  in
+            return true
+        }
+    }
+}
