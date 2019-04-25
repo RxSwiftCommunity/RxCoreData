8a0c82178830e81b2dfafa08973bda4505ebe2ad
bobgodwinx <bobgodwinx@gmail.com>
2019-04-25 18:15:00 +0000

    Cleanup NSManagedObjectContext+Rx extension

--- Sources/NSManagedObjectContext+Rx.swift
+++ Sources/NSManagedObjectContext+Rx.swift
@@ -1,11 +1,3 @@
-//
-//  NSManagedObjectContext+Rx.swift
-//  RxCoreData
-//
-//  Created by Krunoslav Zaher on 5/18/16.
-//  Copyright © 2016 Krunoslav Zaher. All rights reserved.
-//
-
 import Foundation
 import CoreData
 import RxSwift
@@ -20,18 +12,18 @@ public extension Reactive where Base: NSManagedObjectContext {
      - returns: An `Observable` array of `NSManagedObjects` objects that can be bound to a table view.
      */
     func entities<T: NSManagedObject>(fetchRequest: NSFetchRequest<T>,
-                  sectionNameKeyPath: String? = nil,
-                  cacheName: String? = nil) -> Observable<[T]> {
+                                      sectionNameKeyPath: String? = nil,
+                                      cacheName: String? = nil) -> Observable<[T]> {
+        
         return Observable.create { observer in
-			
-			let observerAdapter = FetchedResultsControllerEntityObserver(observer: observer, fetchRequest: fetchRequest, managedObjectContext: self.base, sectionNameKeyPath: sectionNameKeyPath, cacheName: cacheName)
-			
-			return Disposables.create {
-				observerAdapter.dispose()
-			}
+            let observerAdapter = FetchedResultsControllerEntityObserver(observer: observer, fetchRequest: fetchRequest, managedObjectContext: self.base, sectionNameKeyPath: sectionNameKeyPath, cacheName: cacheName)
+            
+            return Disposables.create {
+                observerAdapter.dispose()
+            }
         }
     }
-	
+    
     /**
      Executes a fetch request and returns the fetched section objects as an `Observable` array of `NSFetchedResultsSectionInfo`.
      - parameter fetchRequest: an instance of `NSFetchRequest` to describe the search criteria used to retrieve data from a persistent store
@@ -40,8 +32,9 @@ public extension Reactive where Base: NSManagedObjectContext {
      - returns: An `Observable` array of `NSFetchedResultsSectionInfo` objects that can be bound to a table view.
      */
     func sections<T: NSManagedObject>(fetchRequest: NSFetchRequest<T>,
-                     sectionNameKeyPath: String? = nil,
-                     cacheName: String? = nil) -> Observable<[NSFetchedResultsSectionInfo]> {
+                                      sectionNameKeyPath: String? = nil,
+                                      cacheName: String? = nil) -> Observable<[NSFetchedResultsSectionInfo]> {
+
         return Observable.create { observer in
             let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
                                                  managedObjectContext: self.base,
@@ -60,15 +53,13 @@ public extension Reactive where Base: NSManagedObjectContext {
      - parameter updateAction: a throwing update action
      */
     func performUpdate(updateAction: (NSManagedObjectContext) throws -> Void) throws {
+        
         let privateContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
         privateContext.parent = self.base
         
         try updateAction(privateContext)
-        
         guard privateContext.hasChanges else { return }
-        
         try privateContext.save()
-        
         try self.base.save()
     }
 }
@@ -95,6 +86,7 @@ public extension Reactive where Base: NSManagedObjectContext {
      - parameter persistable: a `Persistable` object
      */
     func delete<P: Persistable>(_ persistable: P) throws {
+        
         if let entity = try get(persistable) {
             self.base.delete(entity)
             
@@ -115,16 +107,14 @@ public extension Reactive where Base: NSManagedObjectContext {
      - returns: An `Observable` array of `Persistable` objects that can be bound to a table view.
      */
     func entities<P: Persistable>(_ type: P.Type = P.self,
-                  predicate: NSPredicate? = nil,
-                     sortDescriptors: [NSSortDescriptor]? = nil) -> Observable<[P]> {
+                                  predicate: NSPredicate? = nil,
+                                  sortDescriptors: [NSSortDescriptor]? = nil) -> Observable<[P]> {
+        
         let fetchRequest: NSFetchRequest<P.T> = NSFetchRequest(entityName: P.entityName)
         fetchRequest.predicate = predicate
         fetchRequest.sortDescriptors = sortDescriptors ?? [NSSortDescriptor(key: P.primaryAttributeName, ascending: true)]
         
-        return entities(fetchRequest: fetchRequest)
-            .map {
-                $0.map(P.init)
-            }
+        return entities(fetchRequest: fetchRequest).map {$0.map(P.init)}
     }
     
     /**
